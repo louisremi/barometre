8f73489e73db857b5727b87084dd741f61f3dc6a
franz <franz@franz-VirtualBox.(none)>
2013-01-23 14:03:17 +0100

    ajout vue par comparaison

--- script/views/QuestionMonth.js
+++ script/views/QuestionMonth.js
@@ -10,12 +10,54 @@ App.Views.QuestionMonth = Backbone.View.extend({
 
 	initialize: function(options) {
 		this.type = options.type;
+		this.compare = false;
+		this.compareViews = [];
+		this.questionsCompareCollection = [];
 	},
 
 	render: function() {
+		var self = this;
 		this.$el.html( this.template({type:this.type}) );
 		this.$percentages = this.$el.find(".percentage span");
 		this.noDataContainer = this.$el.parent().find(".no-data");
+
+		this.$el.parent().find(".more").click(function() {
+			if (!this.compare) {
+				var currentYear = App.ui.model.get("year");
+
+				_.each(_.difference(App.ui.years,[+currentYear]),function(year,index) {
+					var questions = new App.collections.QuestionCollection();
+					questions.setUrl(self.type,year,('0'+App.ui.model.get("month")).slice(-2));
+
+					questions.bind("reset",function() {
+						var question = this.at(0);
+						var view = self.compareViews ? self.compareViews[index] : undefined;
+						if (!view) {
+							view = new App.Views.QuestionMonth({type:self.type,el:$("<ul>")});
+							self.$el.after(view.render().el);
+							view.$el.addClass("answers");
+						}
+						if (question) {
+							view.hookUp(question);
+						} else {
+							view.noData();
+						}
+
+						self.compareViews.push(view);
+
+					},questions);
+
+					questions.fetch();
+					self.questionsCompareCollection.push(questions);
+				});
+
+			} else {
+				_.each(self.compareViews,function(view) {
+					view.remove();
+				})
+			}
+			this.compare = !this.compare;
+		});
 
 		return this;
 	},
@@ -43,6 +85,13 @@ App.Views.QuestionMonth = Backbone.View.extend({
 				wrapper.style.top = Math.round( 85 - ( answer.value || 1 ) * 0.4 ) + "px";
 			}
 		});
+
+		if (this.questionsCompareCollection.length) {
+			_.each(_.difference(App.ui.years,[+App.ui.model.get("year")]),function(year,index) {
+				self.questionsCompareCollection[index].setUrl(self.type,year,('0'+App.ui.model.get("month")).slice(-2));
+				self.questionsCompareCollection[index].fetch();
+			});
+		}
 	},
 
 	noData: function() {
