045ef4ee3c344934d635a045fadbd24bf9471da2
louisremi <lrbabe@gmail.com>
2013-02-09 20:32:22 +0100

    continur implementing actualite questions modifications

--- script/views/QuestionActualite.js
+++ script/views/QuestionActualite.js
@@ -23,36 +23,24 @@ App.Views.QuestionActualite = Backbone.View.extend({
 			.nextAll().remove();
 
 		_( questions ).each(function( question ) {
-			var answers = question.get("answers"),
+			var answers = _.filter( question.get("answers"), function( answer ) {
+					if ( answer.value ) {
+						answer.fontSize = Math.round( ( answer.value || 1 ) * 0.58 + 14 ) + "px";
+						answer.top = Math.round( 105 - ( answer.value || 1 ) * 0.4 ) + "px";
+						return true;
+					}
+				}),
 				nbAnswers = answers.length,
 				answersByLine = nbAnswers % 5 === 0 ? 5 :
 					nbAnswers % 4 === 0 ? 4 :
 					nbAnswers % 3 === 0 ? 3 :
 					4;
 
-			// calculate style of answers
-			_( answers ).map(function( answer ) {
-				answer.fontSize = Math.round( ( answer.value || 1 ) * 0.58 + 14 ) + "px";
-				answer.top = Math.round( 105 - ( answer.value || 1 ) * 0.4 ) + "px";
-			});
-
 			self.$el.append( self.template({
 				question: question.get("title"),
 				answers: answers,
 				answersByLine: answersByLine
 			}) );
-
-			/*var $percentages = self.$el.find(".answers:last .percentage span:last-child");
-
-			// The position
-			_( question.get("answers") ).each(function(answer, i) {
-				if ( $percentages[i] ) {
-					var wrapper = $percentages[i].parentNode;
-
-					wrapper.style.fontSize = Math.round( ( answer.value || 1 ) * 0.58 + 14 ) + "px";
-					wrapper.style.top = Math.round( 105 - ( answer.value || 1 ) * 0.4 ) + "px";
-				}
-			});*/
 		});
 	}
 });
